d29281e13f117ea89c4d01006daf2a62e3073d33
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2025-04-09 10:58:13 -0300

    Adicionando classe Registro para utilizar nos arquivos indexados

---
+++ Codigo/src/model/Registro.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Registro{
+
+    int id;
+    long end;
+
+    public Registro(){
+
+        id = -1;
+        end = -1;
+
+    }
+
+    public Registro(int id, long end){
+
+        this.id = id;
+        this.end = end;
+
+    }
+
+}
