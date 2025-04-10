2d0807c3dc7b29d736418fdce536a4605cec7fa2
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2025-04-10 11:37:15 -0300

    Adicionando CRUD indexado no consoleHelper

---
+++ Codigo/src/repository/CRUDI.java
@@ -0,0 +1,354 @@
+package repository;
+
+import java.io.*;
+
+import util.*;
+import model.*;
+
+public final class CRUDI {
+    
+    private static String arquivo = "data/database/rock.db";
+    private static String arquivoOrdenado = "data/database/final.db";
+
+    private CRUDI(){}//Construtor privado para impedir instanciações
+
+/////////////////////////////////////////////////CREATE//////////////////////////////////////////////////////
+
+    /*
+     * Método para adicionar um registro do arquivo. 
+     * 
+     * Funcionamento: 
+     * -Recebe um objeto "Musica" e organiza o registro.
+     * -O arquivo é aberto, o ponteiro é movido para o início, onde escreve o ultimo índice incerido.
+     * -O ponteiro é movido para o final do arquivo e escreve o registro, que é organizado em: lápide, tamanho do registro e dados.
+     * -O arquivo é fechado.
+     */
+    public static void create(Musica musica, boolean conf) throws FileNotFoundException, IOException{
+
+        Logger.log(LogLevel.INFO, "CREATE chamado!");
+
+        try(RandomAccessFile file = new RandomAccessFile(arquivo, "rw")){
+
+            musica.setIndex(obterProximoId());
+
+            file.seek(0);
+            file.writeInt(musica.getIndex());
+            
+            long endereco = file.length();
+            file.seek(endereco);
+            byte[] array = musica.toByteArray();
+
+            file.writeBoolean(false);
+            file.writeInt(array.length);
+            file.write(array);
+
+            if(conf){
+
+                System.out.println();
+                System.out.println("Música adicionada com sucesso! ID = " + musica.getIndex());
+                System.out.println();
+
+            }
+
+            file.close();
+
+        }catch(IOException e){
+
+            Logger.log(LogLevel.ERROR, "Erro CRUD.create: " + e.getMessage());
+
+        }
+
+    }
+
+/////////////////////////////////////////////////READ//////////////////////////////////////////////////////
+
+    /*
+     * Método para ler todos os registros do arquivo. 
+     * 
+     * Funcionamento: 
+     * -O arquivo é aberto e o ponteiro lê registro por registro.
+     * -Se a lápide estiver falsa, um novo objeto "Musica" é criado com os dados lidos do arquivo e imprimidos na tela.
+     * -Se a lápide estiver verdadeira(registro "removido"), os dados não são imprimidos na tela.
+     * -O arquivo é fechado.
+     */
+    public static void read(char c) throws FileNotFoundException, IOException{
+
+        Logger.log(LogLevel.INFO, "READALL chamado!");
+
+        String arq = "";
+
+        if(c == 'D'){
+
+            arq = arquivo;
+
+        }else{
+
+            arq = arquivoOrdenado;
+
+        }
+
+        try(RandomAccessFile file = new RandomAccessFile(arq, "r")){
+
+            file.readInt();
+
+            while(file.getFilePointer() < file.length()){
+
+                boolean lapide = file.readBoolean();
+                int tamanho = file.readInt();
+                byte[] array = new byte[tamanho];
+                file.readFully(array);
+
+                Musica nova = new Musica();
+                nova.fromByteArray(array);
+
+                if(!lapide){
+
+                    System.out.println(nova);
+
+                }
+
+            }
+
+            file.close();
+
+        }catch(IOException e){
+
+            Logger.log(LogLevel.ERROR, "Erro CRUD.readAll: " + e.getMessage());
+
+        }
+
+    }
+
+    /*
+     * Método para ler um registro do arquivo. 
+     * 
+     * Funcionamento: 
+     * -Recebe o índice do registro desejado.
+     * -O arquivo é aberto e o ponteiro lê registro por registro até achar o índice procurado.
+     * -Se o índice for encontrado.
+     * -Se a lápide estiver falsa, um novo objeto "Musica" é criado com os dados lidos do arquivo e retornado.
+     * -Se a lápide estiver verdadeira(registro "removido"), o método retorna null(registro não encontrado).
+     * -Se o índice não for encontrado, o método retorna null(registro não encontrado).
+     * -O arquivo é fechado.
+     */
+    public static Musica read(int id) throws IOException{
+
+        Logger.log(LogLevel.INFO, "READ chamado!");
+
+        try(RandomAccessFile file = new RandomAccessFile(arquivo, "r")){
+
+            file.readInt();
+
+            while(file.getFilePointer() < file.length()){
+
+                boolean lapide = file.readBoolean();
+                int tamanho = file.readInt();
+                byte[] array = new byte[tamanho];
+                file.readFully(array);
+
+                Musica nova = new Musica();
+                nova.fromByteArray(array);
+
+                if(nova.getIndex() == id){
+
+                    if(!lapide){
+
+                    
+                        return nova;
+
+                    }
+
+                }
+
+            }
+
+            file.close();
+
+        }catch(IOException e){
+
+            Logger.log(LogLevel.ERROR, "Erro CRUD.read: " + e.getMessage());
+
+        }
+
+        return null;
+
+    }
+
+/////////////////////////////////////////////////UPDATE//////////////////////////////////////////////////////
+
+    /*
+     * Método para atualizar um registro do arquivo. 
+     * 
+     * Funcionamento: 
+     * -Recebe o id da música a ser atualizada e o novo objeto de música.
+     * -Identifica se a música está "viva" ou "morta" e se o novo tamanho é menor ou igual ao antigo. Se estiver "morta retorna false."
+     * -Se o novo tamanho é menor ou igual ao antigo, o registro é atualizado.
+     * -Se o novo tamanho é maior que o antigo, a lápide é marcada como verdadeira e um novo registro é adicionado no final do arquivo com o mesmo índice da música a ser atualizada.
+     */
+     public static boolean update(int id, Musica novaMusica) throws IOException{
+
+        Logger.log(LogLevel.INFO, "UPDATE chamado!");
+
+        try(RandomAccessFile file = new RandomAccessFile(arquivo, "rw")){
+
+            file.readInt(); 
+
+            while(file.getFilePointer() < file.length()){
+
+                long posicao = file.getFilePointer();
+                boolean lapide = file.readBoolean();
+                int tamanhoAntigo = file.readInt();
+                byte[] array = new byte[tamanhoAntigo];
+                file.readFully(array);
+
+                Musica musicaAntiga = new Musica();
+                musicaAntiga.fromByteArray(array);
+
+                if(musicaAntiga.getIndex() == id && !lapide){
+
+                    byte[] novoArray = novaMusica.toByteArray();
+                    int tamanhoNovo = novoArray.length;
+
+                    if(tamanhoNovo <= tamanhoAntigo){
+
+                        file.seek(posicao + 1 + 4); 
+                        file.write(novoArray);
+
+                        if(tamanhoNovo < tamanhoAntigo){
+
+                            file.write(new byte[tamanhoAntigo - tamanhoNovo]);
+
+                        }
+
+                    }else{
+
+                        file.seek(posicao);
+                        file.writeBoolean(true);
+
+                        file.seek(file.length());
+                        file.writeBoolean(false); 
+                        file.writeInt(novoArray.length);
+                        file.write(novoArray);                        
+                    }
+
+                    return true;
+
+                }
+
+            }
+
+        }catch(IOException e){
+
+            Logger.log(LogLevel.ERROR, "Erro CRUD.update: " + e.getMessage());
+
+        }
+
+        return false;
+
+    }
+ 
+/////////////////////////////////////////////////DELETE//////////////////////////////////////////////////////
+
+    /*
+     * Método para "deletar" um registro do arquivo. 
+     * 
+     * Funcionamento: 
+     * -Recebe o índice do registro que se deseja "deletar".
+     * -O arquivo é aberto e o ponteiro lê registro por registro até achar o índice procurado.
+     * -Se o índice for encontrado, o ponteiro retorna até a lápide do registro .
+     * -Se a lápide estiver falsa(registro não removido) ela é marcada como verdadeira e o método retorna true(registro "removido").
+     * -Se a lápide estiver verdadeira(registro removido) o método retorna false(registro já "removido").
+     * -Se o índice não for encontrado, o método retorna false(registro não encontrado).
+     * -O arquivo é fechado.
+     */
+    public static boolean delete(int id) throws FileNotFoundException, IOException{
+
+        Logger.log(LogLevel.INFO, "DELETE chamado!");
+
+        try(RandomAccessFile file = new RandomAccessFile(arquivo, "rw")){
+
+            file.readInt();
+
+            while(file.getFilePointer() < file.length()){
+
+                long posicao = file.getFilePointer();
+                boolean lapide = file.readBoolean();
+                int tamanho = file.readInt();
+                byte[] array = new byte[tamanho];
+                file.readFully(array);
+
+                Musica nova = new Musica();
+                nova.fromByteArray(array);
+
+                if(nova.getIndex() == id && !lapide){
+
+                    file.seek(posicao);
+                    file.writeBoolean(true);
+
+                    return true;
+
+                }
+
+            }
+
+            file.close();
+
+        }catch(IOException e){
+
+            Logger.log(LogLevel.ERROR, "Erro CRUD.delete: " + e.getMessage());
+
+        }
+
+        return false;
+
+    }
+
+/////////////////////////////////////////////////AUXILIARES//////////////////////////////////////////////////////
+
+    /*
+     * Método para obter o próximo índice. 
+     * 
+     * Funcionamento: 
+     * -O arquivo é aberto e o ponteiro é movido para o início do arquivo.
+     * -O ponteiro lê o ultimo índice inserido, soma 1 e retorna.
+     * -O arquivo é fechado.
+     */
+    private static int obterProximoId() throws IOException{
+
+        try(RandomAccessFile file = new RandomAccessFile(arquivo, "rw")){
+
+            if(file.length() == 0){
+
+                return 0;
+
+            }else{
+
+                file.seek(0);
+                int ultimoId = file.readInt();
+                return ultimoId + 1;
+
+            }
+
+        }
+
+    }
+
+    /*
+     * Método para apagar database
+     * 
+     * Funcionamento: 
+     * -Abre o arquivo .db e apaga todo o conteúdo
+     */
+    public static void reiniciarBD() throws FileNotFoundException, IOException{
+
+        try(RandomAccessFile file = new RandomAccessFile(arquivo, "rw")){
+
+            file.setLength(0);
+
+            file.close();
+
+        }
+
+    }
+
+}
--- Codigo/src/util/ConsoleHelper.java
+++ Codigo/src/util/ConsoleHelper.java
@@ -14,7 +14,20 @@ import algorithms.sorting.IntercalacaoBalanceada;
 
 public class ConsoleHelper {
     
+    private static Scanner scan;
+
     private ConsoleHelper(){}
+
+    private static void tipoMenu(){
+
+        System.out.println("=========================BEM=VINDO=AO=DB=DO=ROCK=N=ROLL=========================");
+        System.out.println("Escolha o tipo de CRUD que será utilizado:");
+        System.out.println("1 -> Sequencial");
+        System.out.println("2 -> Indexado");
+        System.out.println("0 -> Encerrar");
+        System.out.println("================================================================================");
+
+    }
 
     /*
      * Método para imprimir o menu de escolhas do usuário
@@ -22,19 +35,48 @@ public class ConsoleHelper {
      * Funcionamento: 
      * -Imprime na tela todas as opções de ações do usuário
      */
-    private static void menu(){
+    private static void menu(int tipo){
 
-        System.out.println("=========================BEM=VINDO=AO=DB=DO=ROCK=N=ROLL=========================");
-        System.out.println("Escolha uma opção: ");
-        System.out.println("1 -> Carregar base de dados no arquivo");
-        System.out.println("2 -> Criar um registro");
-        System.out.println("3 -> Ler um registro");
-        System.out.println("4 -> Atualizar registro");
-        System.out.println("5 -> Deletar registro");
-        System.out.println("6 -> Ler todos os registros");
-        System.out.println("7 -> Ordenar arquivo");
-        System.out.println("8 -> Ler arquivo ordenado");
-        System.out.println("0 -> Encerrar");
+        if(tipo == 1){
+
+            System.out.println("===================================SEQUENCIAL===================================");
+            System.out.println("Escolha uma opção: ");
+            System.out.println("1 -> Carregar base de dados no arquivo");
+            System.out.println("2 -> Criar um registro");
+            System.out.println("3 -> Ler um registro");
+            System.out.println("4 -> Atualizar registro");
+            System.out.println("5 -> Deletar registro");
+            System.out.println("6 -> Ler todos os registros");
+            System.out.println("7 -> Ordenar arquivo");
+            System.out.println("8 -> Ler arquivo ordenado");
+            System.out.println("0 -> Voltar");
+            System.out.println("================================================================================");
+
+        }else if(tipo == 2){
+
+            System.out.println("====================================INDEXADO====================================");
+            System.out.println("Escolha uma opção: ");
+            System.out.println("1 -> Carregar base de dados no arquivo");
+            System.out.println("2 -> Criar um registro");
+            System.out.println("3 -> Ler um registro");
+            System.out.println("4 -> Atualizar registro");
+            System.out.println("5 -> Deletar registro");
+            System.out.println("6 -> Ler todos os registros");
+            System.out.println("0 -> Voltar");
+            System.out.println("================================================================================");
+
+        }
+
+    }
+
+    private static void menuDeIndices(){
+
+        System.out.println("================================================================================");
+        System.out.println("Escolha o tipo de índice que será utilizado:");
+        System.out.println("1 -> Árvore B");
+        System.out.println("2 -> Tabela Hash Extensível");
+        System.out.println("3 -> Lista Invertida");
+        System.out.println("0 -> Voltar");
         System.out.println("================================================================================");
 
     }
@@ -52,6 +94,46 @@ public class ConsoleHelper {
 
     }
 
+    public static void run() throws FileNotFoundException, IOException{
+
+        scan = new Scanner(System.in);
+
+        int tipoDeCRUD;
+
+        do{
+
+            clear();
+            tipoMenu();
+
+            tipoDeCRUD = scan.nextInt();
+
+            if(tipoDeCRUD != 0){
+
+                if(tipoDeCRUD == 1){
+
+                    clear();
+                    runSequencial();
+    
+                }else if(tipoDeCRUD == 2){
+    
+                    clear();
+                    runIndexado();
+    
+                }else{
+    
+                    clear();
+                    System.out.println("Opção inválida!");
+    
+                }
+
+            }
+
+        }while(tipoDeCRUD != 0);
+
+        scan.close();
+
+    }
+
     /*
      * Método para a interação com o usuário
      * 
@@ -59,17 +141,16 @@ public class ConsoleHelper {
      * -Chama o método menu e recebe a opção do usuário
      * -Recebe informações do usuário e chama o método necessário pra cada caso
      */
-    public static void run() throws FileNotFoundException, IOException{
+    public static void runSequencial() throws FileNotFoundException, IOException{
 
-        Scanner scan = new Scanner(System.in);
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy"); //Reconhece o formato que a data será lida e exibida ao usuário para utilizar nas transformações para Timestamp
     
         int op = -1;
         int id = -1;
     
-        while(op != 0){
+        do{
     
-            menu();
+            menu(1);
     
             op = scan.nextInt();
     
@@ -114,7 +195,7 @@ public class ConsoleHelper {
                     scan.nextLine();
                     
                     System.out.print("Artistas relacionados (separados por vírgula): ");
-                    String[] artistas = scan.nextLine().split(",\\s*");
+                    String[] artistas = scan.nextLine().split(",\s*");
 
                     Musica addNovaMusica = new Musica(nome, artista, data, duracao, artistas);
                     CRUD.create(addNovaMusica, true);
@@ -177,7 +258,7 @@ public class ConsoleHelper {
                     scan.nextLine();
                     
                     System.out.print("Novos artistas relacionados (separados por vírgula): ");
-                    String[] fArtists = scan.nextLine().split(",\\s*");
+                    String[] fArtists = scan.nextLine().split(",\s*");
 
                     Musica novaMusica = new Musica(id, name, artist, date, length, fArtists);
 
@@ -245,17 +326,218 @@ public class ConsoleHelper {
 
                     break;
                 
-                    default:
+                default:
                     
                     clear();
-                    op = 0;
+                    System.out.println("Opção inválida!");
                     break;
     
             }
     
-        }
+        }while(op != 0);
+
+    }
+
+    private static void runIndexado() throws FileNotFoundException, IOException{
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy"); //Reconhece o formato que a data será lida e exibida ao usuário para utilizar nas transformações para Timestamp
     
-        scan.close();
+        int op = -1;
+        int id = -1;
+        int indice = -1;
+
+        do{
+
+            clear();
+            menuDeIndices();
+            indice = scan.nextInt();
+
+            if(indice != 0){
+
+                if(indice >= 1 && indice <= 3){
+
+                    do{
+        
+                        clear();
+                        menu(2);
+                
+                        op = scan.nextInt();
+                
+                        switch(op){
+                
+                            case 0:
+            
+                                break;
+                            
+                            case 1:
+                
+                                clear();
+                                CsvHandler.preencherCatalogo();
+                                break;
+            
+                            case 2:
+                            
+                                scan.nextLine();
+                                clear();
+                
+                                System.out.print("Nome: ");
+                                String nome = scan.nextLine();
+                                System.out.print("Artista: ");
+                                String artista = scan.nextLine();
+                                System.out.print("Data dd-MM-yyyy: ");
+                                String dataStr = scan.nextLine();
+                                long data = 0;
+            
+                                try{
+            
+                                    Date parsedDate = dateFormat.parse(dataStr);
+                                    data = parsedDate.getTime();
+            
+                                }catch(ParseException e){
+            
+                                    System.out.println("Erro: " + e.getMessage());
+                                    
+                                }
+            
+                                System.out.print("Duração: ");
+                                double duracao = scan.nextDouble();
+                                scan.nextLine();
+                                
+                                System.out.print("Artistas relacionados (separados por vírgula): ");
+                                String[] artistas = scan.nextLine().split(",\\s*");
+            
+                                Musica addNovaMusica = new Musica(nome, artista, data, duracao, artistas);
+                                CRUD.create(addNovaMusica, true);
+                    
+                                break;
+                            
+                            case 3:
+                
+                                clear();
+                                System.out.print("Informe o index da música procurada: ");
+                                id = scan.nextInt();
+            
+                                Musica musica = CRUD.read(id);
+                
+                                clear();
+            
+                
+                                if(musica == null){
+                
+                                    System.out.println();
+                                    System.out.println("Registro não encontrado!");
+                                    System.out.println();
+                
+                                }else{
+                
+                                    System.out.println(musica);
+                
+                                }
+                
+                                break;
+                
+                            case 4:
+                
+                                clear();
+                                System.out.print("Informe o index da música a ser atualizada: ");
+                                id = scan.nextInt();
+                                scan.nextLine();
+            
+                                System.out.print("Novo nome: ");
+                                String name = scan.nextLine();
+                                System.out.print("Novo artista: ");
+                                String artist = scan.nextLine();
+                                System.out.print("Nova data dd-MM-yyyy: ");
+                                String dateStr = scan.nextLine();
+                                long date = 0;
+            
+                                try{
+            
+                                    Date parsedDate = dateFormat.parse(dateStr);
+                                    date = parsedDate.getTime();
+                                    
+                                }catch(ParseException e){
+            
+                                    System.out.println("Erro: " + e.getMessage());
+            
+                                }
+            
+                                System.out.print("Nova duração: ");
+                                double length = scan.nextDouble();
+                                scan.nextLine();
+                                
+                                System.out.print("Novos artistas relacionados (separados por vírgula): ");
+                                String[] fArtists = scan.nextLine().split(",\\s*");
+            
+                                Musica novaMusica = new Musica(id, name, artist, date, length, fArtists);
+            
+                                if(CRUD.update(id, novaMusica)){
+            
+                                    System.out.println();
+                                    System.out.println("Registro atualizado com sucesso!");
+                                    System.out.println();
+            
+                                }else{
+            
+                                    System.out.println();
+                                    System.out.println("Erro ao atualizar o registro!");
+                                    System.out.println();
+            
+                                }
+                                
+                                break;
+                
+                            case 5:
+                
+                                clear();
+                                System.out.print("Informe o index da música que deseja remover: ");
+                                id = scan.nextInt();
+            
+                                clear();
+                                
+                                if(CRUD.delete(id)){
+            
+                                    System.out.println();
+                                    System.out.println("Música removida com sucesso!");
+                                    System.out.println();
+            
+                                }else{
+            
+                                    System.out.println();
+                                    System.out.println("Erro ao remover a música!");
+                                    System.out.println();
+            
+                                }
+            
+                                System.out.println();
+            
+                                break;
+            
+                            case 6:
+            
+                                clear();
+                                CRUD.read('D');
+                                break;
+                            
+                            default:
+                                
+                                clear();
+                                System.out.println("Opção inválida!");
+                                break;
+                
+                        }
+                
+                    }while(op != 0);
+    
+                }else{
+    
+                    System.out.println("Opção inválida!");
+    
+                }
+
+            }
+
+        }while(indice != 0);
 
     }
 
